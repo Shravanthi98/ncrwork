4df15029d80480c6c299d0189c327b336489f670
Shravanthi98 <shravanthi.musti@yahoo.com>
2019-02-11 20:54:03 +0530

    To delete a char from string

--- C-Assignment4/c4ques10.c
+++ C-Assignment4/c4ques10.c
@@ -1,32 +1,38 @@
 #include<stdio.h>
-#include<stdlib.h>
 void delete(char * , char);
 int main()
 {
-char *s1 = NULL;
+int n;
+char *s1;
 char c1;
+printf("Enter the number");
+scanf("%d",&n);
 printf("Enter the string");
-gets(s1);
-printf("\n");
+s1 = (char*)malloc(n*sizeof(char));
+scanf("%s",s1);
 printf("Enter the character");
-getchar(c1);
+scanf(" %c",&c1);
 printf("\n");
 delete(s1,c1);
 return 0;
 }
 void delete(char *s1 , char c1)
 {
-while(*s1!='\0')
+char *s2;
+s2 = s1;
+int n,i,c=0;
+n = strlen(s1);
+while(*s2!='\0')
 {
-if(*s1 == c1)
+if(*s2 == c1)
 {
-while(*s1!='\0')
+for(i=0;i<n-c;i++)
 {
-*s1 = *(++s1);
+*(s2+i) = *(s2+i+1);
 }
 }
-else
-s1++;
+s2++;
+c++;
 }
 printf("%s",s1);
 }
