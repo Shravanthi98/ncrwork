65b002f7535156a0f99ce8eafc6bf6c23d68ad11
Shravanthi98 <shravanthi.musti@yahoo.com>
2019-03-07 00:23:04 +0530

    Rest of 3rd assgn

---
+++ C-Assignment3/c2ques5.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+
+{
+	
+int n,temp,rem;
+	
+int res = 0;
+printf("Enter a number");
+	
+scanf("%d",&n);
+	
+temp = n;
+	
+while(temp!=0)
+
+{
+		
+rem = temp%10;
+		
+res = (res*10)+rem;
+		
+temp = temp/10;
+
+}
+
+printf("%d",res);
+	
+return 0;
+
+}
