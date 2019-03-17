445065eed9e2336282ab1f29fa0260b72b2007e4
Shravanthi98 <shravanthi.musti@yahoo.com>
2019-03-17 23:34:00 +0530

    Cpp Assn1

--- Cpp-Assgn1/quest3.cpp
+++ Cpp-Assgn1/quest3.cpp
@@ -2,12 +2,15 @@
 using namespace std;
 class complex
 {
+//private
 float real,imag;
 public:
+//default constructor
 complex()
 {
 real=imag=0;
 }
+//parameterised constructor
 complex(float x)
 {
 real=imag=x;
@@ -17,15 +20,18 @@ complex(float p,float q)
 real=p;
 imag=q;
 }
+//Display complex numbers
 void display(complex c1)
 {
 cout<<"add="<<real<<"+j"<<imag<<endl;
 cout<<"mul="<<c1.real<<"+j"<<c1.imag<<endl;
 
 }
+//friend functions to add and multiply
 friend complex add(complex c1 ,complex c2);
 friend complex multiply(complex z ,complex c);
 };
+//friend function definition
 complex add(complex x,complex y)
 {
 complex temp;
@@ -44,7 +50,18 @@ return temp;
 
 int main()
 {
-complex c1(3,2),c2(8),c3,c4;
+float num1, num2;
+cout<<"Enter the numbers to add and multiply"<<endl;
+cin>>num1>>num2;
+if(((num1>=0)&&(num1<=9))||((num2>=0)&&(num2<=9)))
+{
+}
+else
+{
+cout<<"Invalid input, Please enter numbers in the range of 0-9"<<endl;
+exit(0);
+}
+complex c1(num1,num2),c2(num1),c3,c4;  // Constructors are invoked
 c3=add(c1,c2);
 c4=multiply(c1,c2);
 c3.display(c4);
